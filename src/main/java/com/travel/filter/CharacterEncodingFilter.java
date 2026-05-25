fa4b05a74cce317e4c21815b8cc70a39d4b145fc
没有派大星 <3125856785@qq.com>
2026-05-25 12:50:19 +0800

    修复BUG

--- src/main/java/com/travel/filter/CharacterEncodingFilter.java
+++ src/main/java/com/travel/filter/CharacterEncodingFilter.java
@@ -17,7 +17,6 @@ public class CharacterEncodingFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html;charset=UTF-8");
         chain.doFilter(request, response);
     }
 }
